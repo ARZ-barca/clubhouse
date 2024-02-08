1c365dca8fef13cd4c3ad6e51f5043cd7d219c87
alireza zbh <alireza.zabihi@yahoo.com>
2024-02-08 23:49:52 +0330

    add member and admin attributes to authors

--- models/author.js
+++ models/author.js
@@ -4,6 +4,8 @@ const authorSchema = new mongoose.Schema({
   firstName: { type: String, minLength: 3, maxLength: 50, required: true },
   lastName: { type: String, minLength: 3, maxLength: 500, required: true },
   passwordHash: { type: String, required: true },
+  member: { type: Boolean, default: true },
+  admin: { type: Boolean, default: true },
 });
 
 authorSchema.virtual("fullName").get(function () {
