c874707643e9a601adea6c815fe1d9d8bbaddbf2
alireza zbh <alireza.zabihi@yahoo.com>
2024-02-20 20:36:20 +0330

    add date foramatting for messages

--- models/message.js
+++ models/message.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 
+const { DateTime } = require("luxon");
+
 const messageSchema = new mongoose.Schema({
   title: { type: String, maxLength: 100, required: true },
   content: { type: String, maxLength: 1000, required: true },
-  createdAt: { type: String, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
   author: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
+});
+
+// todo
+
+messageSchema.virtual("createdAt_isodate").get(function () {
+  const dt = DateTime.fromJSDate(this.createdAt);
+  console.log(this.createdAt);
+  return dt.toISODate();
 });
 
 module.exports = mongoose.model("Message", messageSchema);
